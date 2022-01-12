251a23620aabff5891090902a57162a20fc52d4b
Lance Pollard <lp@elk.fm>
2022-01-12 08:16:50 -0800

    large primes

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const output = {}
 let i = 0n
 let term
 while (i < 262144n) {
-  [i, term] = compile(i, 131111n, 8)
+  [i, term] = compile(i, 177750093897979823770061456102763834271n, 64)
   if (output[term]) {
     throw new Error(`Duplicate: ${term} originally at index ${output[term]}, now at index ${i}`)
   }
