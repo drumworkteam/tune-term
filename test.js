61c58a51a7ce0e2832c4c3b36885cb005a1077a1
Lance Pollard <lp@elk.fm>
2022-01-12 18:36:08 -0800

    power of 2

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ const compile = require('./form')
 const output = {}
 
 let i = 0n
-let term
-while (i < 262144n) {
-  [i, term] = compile(i, 177750093897979823770061456102763834271n, 64)
+while (i < 65536n) {
+  let term = compile(i, 6500000000000650000000000065000000000006500000000000n, 64)
   if (output[term]) {
     throw new Error(`Duplicate: ${term} originally at index ${output[term]}, now at index ${i}`)
   }
   console.log(term)
   output[term] = i
+  i++
 }
---
+++ tone.js
@@ -0,0 +1,39 @@
+const i = [
+  'mi',
+  'ma',
+  'mo',
+  'di',
+  'da',
+  'do',
+  'ki',
+  'ka',
+  'ko',
+  'si',
+  'sa',
+  'so',
+  'vi',
+  'va',
+  'vo',
+  'la',
+]
+
+const o = [
+  'ne',
+  'nu',
+  'be',
+  'bu',
+  'te',
+  'tu',
+  'ke',
+  'ku',
+  'su',
+  'ze',
+  're',
+  'ru',
+  've',
+  'vu',
+  'xe',
+  'xu',
+]
+
+module.exports = { i, o }
